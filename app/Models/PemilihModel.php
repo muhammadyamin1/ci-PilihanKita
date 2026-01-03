e13b2668bb56a0ff7196587302a270bb046145a6
Muhammad Yamin <46164109+muhammadyamin1@users.noreply.github.com>
2026-01-03 12:08:51 +0700

    Memperbaiki Error Controller Pemilih

--- app/Models/PemilihModel.php
+++ app/Models/PemilihModel.php
@@ -1,10 +1,9 @@
 <?php
 
 namespace App\Models;
-
 use CodeIgniter\Model;
 
-class UserModel extends Model
+class PemilihModel extends Model
 {
     protected $table            = 'users';
     protected $primaryKey       = 'id';
